e668a74b811df9a69fa27e6948e2577913978798
Tsai <czn826@gmail.com>
2022-12-16 07:00:29 +1100

    first files listing test

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown';
 
-import about from '../../blogs/2022/新十条.md'
+import './HomePage.css'
+
+// import about from '../../blogs/2022/新十条.md'
+import about from '../../blogs/about.md'
+
+// https://stackoverflow.com/questions/65587431/load-a-list-of-internal-files-in-react
+const webpackContext = require.context('../../blogs/', false, /\.md$/)
+const filenames = webpackContext.keys()
+// const key_value_pairs = filenames.map(name => [name.match(/\/(\w+)\.md$/)[1], webpackContext(name)])
+// const messages = Object.fromEntries(key_value_pairs)
+
 
 const HomePage = () => {
 
@@ -18,9 +28,17 @@ const HomePage = () => {
 
 
   return (
-    <div>
-      <ReactMarkdown># Home *page*!</ReactMarkdown>
+    <div className='Homepage'>
+      <div>
+        {filenames.map((title, titleIndex) => {
+          return (
+            <p>{title}</p>
+          )
+        })}
+      </div>
+      {/* <ReactMarkdown># Home *page*!</ReactMarkdown> */}
       <ReactMarkdown children={readable.md} />
+
     </div>
   )
 }
