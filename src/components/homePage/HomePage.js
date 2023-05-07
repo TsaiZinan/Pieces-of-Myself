5d96c50ddb4f005779d4feb91a66742cc5d8e01f
Tsai <czn826@gmail.com>
2023-05-07 16:21:14 +1000

    createHashRouter & RouterProvider, change path

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -33,15 +33,15 @@ const HomePage = () => {
 
 
   return (
-    <Router>
+    // <Router>
       <div className='homepage'>
 
         <div className='homepage-nav'>
 
-          <Link to='/Pieces-of-Myself/' className='homepage-nav-text'>
+          <Link to='/' className='homepage-nav-text'>
             <p>Home</p>
           </Link>
-          <Link to='/Pieces-of-Myself/about' className='homepage-nav-text'>
+          <Link to='/about' className='homepage-nav-text'>
             <p>About</p>
           </Link>
 
@@ -55,8 +55,8 @@ const HomePage = () => {
           {/* <div className='homepage-about'>
             <Route path="/Pieces-of-Myself/about" element={<MdDisplay inputMdText={about} />} />
           </div> */}
-          <Route path="/Pieces-of-Myself/about" element={<AboutPage about={about} />} />
-          <Route path="/Pieces-of-Myself/" element={<TableOfContents />} />
+          <Route path="/about" element={<AboutPage about={about} />} />
+          <Route path="/" element={<TableOfContents />} />
           <Route path="/Pieces-of-Myself/static/media/:path" element={<ArticlePage />} />
         </Routes>
 
@@ -67,7 +67,7 @@ const HomePage = () => {
 
 
       </div>
-    </Router>
+    // </Router>
 
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
